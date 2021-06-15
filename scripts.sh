ba71018d8990a17a339ceedb1035e4947dce30ea
aj-thinklets <agharajubin@gmail.com>
2021-06-15 20:42:16 +0530

    day-2

--- scripts.sh
+++ scripts.sh
@@ -1,6 +1,26 @@
+1. Quick setup of webserver on linux 
+------------------------------------
 #!/bin/bash
 yum update -y
 yum install httpd
 echo "<h1>Hello - $(hostname -f)" > /var/www/html/index.html
 systemctl start httpd
 systemctl enable httpd
+
+
+
+2. Installing Cloud Watch Agent on Amazon Linux 2
+https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/QuickStartEC2Instance.html
+----------------------------------------------
+#!/bin/bash
+yum update -y
+yum install -y awslogs
+sudo service awslogs start
+sudo systemctl start awslogsd
+sudo chkconfig awslogs on
+sudo systemctl enable awslogsd.service
+
+#Check the /var/log/awslogs.log file for errors logged when starting the service.
+#Change the region to meet yours - vi /etc/awslogs/awscli/conf
+
+
